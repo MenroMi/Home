0841d75073fd3522558e2cdd2de3fe61b6211665
GECOS <cyryl@localhost.localdomain>
2022-03-15 20:55:02 +0100

    hello

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
 print('x')
 print('y')
+print('x')
+
+code -r hello.py  # uruchomienie plikw vsc
+git add -A . # dodaj do repo wszystkie pliki z bieżącego katalogu
+git add nazwa_pliku # dodaj do repo dany plik z bieżącego katalogu
+git commit -m 'xDD' # dodanie opisu dla twojego pliku
+git push # wypchnięcie zmian w pliku na githuba
+ ===========
+ git diff # pokazuje wszystkie zmiany wprowadzone między aktualną wersją pliku a wcześniejszym commitem
